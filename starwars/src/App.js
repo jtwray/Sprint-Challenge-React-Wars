69bc8aabe849711e06ce26abceae57aaa853c134
jtwray <tuckerwray84@gmail.com>
2019-06-08 15:27:12 -0400

    Defines props.character.'attributes' on component Character.js passed from render in App.js setState to match changes in DOM against data pulled from online URL

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Character from "./components/Character";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      starwarsChars: [],
-      name:"",
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			starwarsChars: [],
+			
+		};
+	}
 
-  componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
-  }
+	componentDidMount() {
+		this.getCharacters("https://swapi.co/api/people/");
+	}
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
+	getCharacters = URL => {
+		// feel free to research what this code is doing.
+		// At a high level we are calling an API to fetch some starwars data from the open web.
+		// We then take that data and resolve it our state.
+		fetch(URL)
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				this.setState({ starwarsChars: data.results });
+			})
+			.catch(err => {
+				throw new Error(err);
+			});
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <h1 className="Header">React Wars</h1>
-       
-        <div>{this.state.name}
-					{this.state.starwarsChars.map((swcharacter) => {
-                        return <Character character={swcharacter}
-                          />;
+	render() {
+		return (
+			<div className='App'>
+				<h1 className='Header'>React Wars</h1>
+
+				<div>
+					
+					{this.state.starwarsChars.map(swcharacter => {
+						return <Character character={swcharacter} />;
 					})}
 				</div>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
