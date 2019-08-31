34167f85e93fb6c20fe344595e7a4051ec732bda
jtwray <tuckerwray84@gmail.com>
2019-08-30 22:11:25 -0400

    refactors project to include hooks. removes class component. uses effect Hook to make axios call to starwars api. sets a characters state with the state hook. maps over the state array of characters to return a character component for each of them. imports styled componenets. initial styling with css n js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,21 +1,71 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import styled from "styled-components";
+import {Character} from "./components/Character";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+import "./App.css";
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+export const App = () => {
+	let [characters, setCharacters] = useState();
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get(`https://swapi.co/api/people/`)
+			// Which we then set to state
+			.then((res) => {
+				setCharacters(res.data.results);
+				console.log(`characters ${characters}`);
+			})
+			// Always include error handling
+			.catch((error) => {
+				console.error();
+			});
+	}, []);
 
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
+	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
 
-
-export default App;
+	return (
+		<div className="App">
+      <Header>
+      React Wars
+      </Header>
+			{characters ? (
+				characters.map((character,i) => {
+          return <Character character={character} key={`${character.toString()}${i}`}/>;
+				})
+			) : (
+				<div>Loading...</div>
+			)}
+		</div>
+	);
+};
+const Header = styled.header`
+  cursor: pointer;
+  font-size:3rem;
+  border-radius:10px;
+  color: #443e3e;
+	text-shadow: 1px 1px 5px #fff;
+	position: fixed;
+	right: 35%;
+	top: 1%;
+	z-index: 3;
+	text-shadow: 5px 5px 3px #fff, 4.9px 4.9px 3px #c78352,
+		4.8px 4.8px 3px #c78352, 4.7px 4.7px 3px #c78352, 4.6px 4.6px 3px #c78352,
+		4.5px 4.5px 3px #c78352, 4.4px 4.4px 3px #c78352, 4.3px 4.3px 3px #c78352,
+		4.2px 4.2px 3px #c78352, 4.1px 4.1px 3px #c78352, 4px 4px 3px #c78352,
+		3.9px 3.9px 3px #c78352, 3.5px 3.5px 3px #c78352, 3.3px 3.3px 3px #c78352,
+		3.2px 3.2px 3px #c78352, 3.7px 3.7px 3px #c78352, 3.6px 3.6px 3px #c78352,
+		3.5px 3.5px 3px #c78352, 3.4px 3.4px 3px #c78352, 3.3px 3.3px 3px #c78352,
+		3.2px 3.2px 3px #c78352, 3.1px 3.1px 3px #c78352, 3px 3px 3px #c78352,
+		2.9px 2.9px 3px #c78352;
+  transition: 0.5s all ease-out;
+  &:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`;
