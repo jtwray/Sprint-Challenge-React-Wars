08ecf0700e8c40aac2dfe1183e3517b53facd349
jtwray <tuckerwray84@gmail.com>
2019-09-02 03:01:11 -0400

    Begins adding a React Carousel component. styles character cards and headers, uses styled-components css-n-js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,26 +1,80 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
 import styled from "styled-components";
 import {Character} from "./components/Character";
+import {Carousel} from "./components/Carousel";
+import c3p0 from "./static/images/c3p0.jpg";
+import Luke from "./static/images/Luke_skywalker (1).jpg";
+import R2D2 from "./static/images/R2D2.jpeg"
 
 import "./App.css";
 
 export const App = () => {
-	let [characters, setCharacters] = useState();
+	const [characters, setCharacters] = useState([]);
 
 	useEffect(() => {
 		axios
-			.get(`https://swapi.co/api/people/`)
-			// Which we then set to state
+			.get("https://swapi.co/api/people/")
+
 			.then((res) => {
 				setCharacters(res.data.results);
-				console.log(`characters ${characters}`);
 			})
-			// Always include error handling
+
 			.catch((error) => {
 				console.error();
 			});
 	}, []);
+	const [character, setCharacter] = useState(characters[0] && characters[0]);
+
+	const updateCharacter = (x) => {
+		x && setCharacter(x);
+	};
+
+	const savedCharacters = useRef("");
+	useEffect(() => {
+		savedCharacters.current = characters;
+	});
+
+	const savedCharacter = useRef({});
+	useEffect(() => {
+		characters[0] && (savedCharacter.current = characters[0]);
+	});
+
+	console.log(savedCharacter.current);
+
+	console.log(savedCharacters.current);
+	useEffect(() => {
+		characters[0] && setCharacter(characters[0]);
+	}, [characters[0]]);
+
+	console.log(
+		"characters:",
+		characters,
+		"characters[0]:",
+		characters[0],
+		"character:",
+		`'${character}'`,
+		"character.name:",
+		`'${character && character}'`,
+		"Object.keys( characters ):",
+		Object.keys(characters),
+		"characters[0]&&characters[0].name:",
+		characters[0] && characters[0].name
+	);
+
+	const charactersRef = useRef(null);
+	charactersRef.current = characters;
+
+	const InitialState = 0;
+	let [charactersIndex, setCharactersIndex] = useState(InitialState);
+	const prevCard = () => {
+		setCharactersIndex((charactersIndex -= 1));
+		console.log(charactersIndex);
+	};
+	const nextCard = () => {
+		setCharactersIndex((charactersIndex += 1));
+		console.log(charactersIndex);
+	};
 
 	// Try to think through what state you'll need for this app before starting. Then build out
 	// the state properties here.
@@ -28,44 +82,164 @@ export const App = () => {
 	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
 	// side effect in a component, you want to think about which state and/or props it should
 	// sync up with, if any.
+	const [heroImages, setHeroImages] = useState([]);
+	const assignImages = () => {
+		// charactersIndex==1? characters[1].heroImage==heroImage[1]:"";
 
+		const newHeroes = [...heroImages, Luke, c3p0, R2D2];
+
+		return console.log(newHeroes);
+	};
+	assignImages();
 	return (
 		<div className="App">
-      <Header>
-      React Wars
-      </Header>
-			{characters ? (
-				characters.map((character,i) => {
-          return <Character character={character} key={`${character.toString()}${i}`}/>;
-				})
-			) : (
-				<div>Loading...</div>
-			)}
+			<HeaderContainer>
+				<Left
+					onClick={(e) => {
+						prevCard();
+					}}>
+					👈
+				</Left>
+				<Header>React Wars</Header>
+				<Right
+					onClick={(e) => {
+						nextCard();
+					}}>
+					{" "}
+					👉
+				</Right>
+			</HeaderContainer>
+			<CardsSlider>
+				<CardsSliderWrapper
+					style={{
+						transform: `translateX(${charactersIndex *
+							(100 / characters.length)}%)`,
+					}}>
+					{characters ? (
+						characters.map((character, i) => {
+							return (
+								<Character
+									character={character}
+									key={`${character.toString()}${i}`}
+								/>
+							);
+						})
+					) : (
+						<div>Loading...</div>
+					)}
+				</CardsSliderWrapper>
+			</CardsSlider>
 		</div>
 	);
 };
+
+const HeaderContainer = styled.div`
+	display: flex;
+	padding: 1rem;
+	align-content: center;
+	display: flex;
+	font-size: 4rem;
+`;
+const Right = styled.div`
+	flex: auto;
+	border-radius: 54px;
+	background-color: white;
+	box-shadow: inset -7px -9px 15px 0px #000002;
+	text-shadow: 1px -1px 0px, 1px 2px 0 black;
+	padding: inherit;
+`;
+const Left = styled.div`
+	flex: auto;
+	text-align: center;
+	display: flex;
+	border-radius: 54px;
+	background-color: white;
+	box-shadow: inset -7px -9px 15px 0px #000002;
+	text-shadow: 1px -1px 0px, 1px 2px 0 black;
+	padding: inherit;
+`;
+const CardsSlider = styled.div`
+	display: flex;
+	position: relative;
+`;
+const CardsSliderWrapper = styled.div`
+	display: flex;
+	position: absolute;
+`;
+
 const Header = styled.header`
-  cursor: pointer;
-  font-size:3rem;
-  border-radius:10px;
-  color: #443e3e;
-	text-shadow: 1px 1px 5px #fff;
-	position: fixed;
-	right: 35%;
-	top: 1%;
-	z-index: 3;
-	text-shadow: 5px 5px 3px #fff, 4.9px 4.9px 3px #c78352,
-		4.8px 4.8px 3px #c78352, 4.7px 4.7px 3px #c78352, 4.6px 4.6px 3px #c78352,
-		4.5px 4.5px 3px #c78352, 4.4px 4.4px 3px #c78352, 4.3px 4.3px 3px #c78352,
-		4.2px 4.2px 3px #c78352, 4.1px 4.1px 3px #c78352, 4px 4px 3px #c78352,
-		3.9px 3.9px 3px #c78352, 3.5px 3.5px 3px #c78352, 3.3px 3.3px 3px #c78352,
-		3.2px 3.2px 3px #c78352, 3.7px 3.7px 3px #c78352, 3.6px 3.6px 3px #c78352,
-		3.5px 3.5px 3px #c78352, 3.4px 3.4px 3px #c78352, 3.3px 3.3px 3px #c78352,
-		3.2px 3.2px 3px #c78352, 3.1px 3.1px 3px #c78352, 3px 3px 3px #c78352,
-		2.9px 2.9px 3px #c78352;
-  transition: 0.5s all ease-out;
-  &:hover {
-    background-color: palevioletred;
-    color: white;
-  }
+	cursor: pointer;
+	font-size: 2rem;
+	border-radius: 10px;
+	color: #443e3e;
+	flex: auto;
+    align-items: center;
+    z-index: 3;
+    display: flex;	
+	text-shadow:	
+	0.1px 0.1px 3px  #FFF,
+	0.2px 0.2px 3px  #EFF,
+	0.3px 0.3px 3px  #DFF,
+	0.4px 0.4px 3px  #CFF,
+	0.5px 0.5px 3px  #BFF,
+	0.6px 0.6px 3px  #AFF,
+	0.7px 0.7px 3px  #9FF,
+	0.8px 0.8px 3px  #8FF,
+	0.9px 0.9px 3px  #7FF,
+	0.0px 0.0px 3px  #6FF,
+	1.1px 1.1px 3px  #5FF,
+	1.2px 1.2px 3px  #4FF,
+	1.3px 1.3px 3px  #3FF,
+	1.4px 1.4px 3px  #2FF,
+	1.5px 1.5px 3px  #1FF,
+	1.6px 1.6px 3px  #0FF,
+	1.7px 1.7px 3px  #FEF,
+	1.8px 1.8px 3px  #FDF,
+	1.9px 1.9px 3px  #FCF,
+	2.5px 2.5px 3px  #FBF,
+	2.6px 2.6px 3px  #FAF,
+	2.7px 2.7px 3px  #F9F,
+	2.8px 2.8px 3px  #F8F,
+	2.9px 2.9px 3px  #F7F,
+	3.0px 3.0px 3px  #F6F,
+	3px 3px 3px #c78352,
+	3.1px 3.1px 3px #c78352,
+	3.2px 3.2px 3px #c78352,
+	3.2px 3.2px 3px #c78352,
+	3.3px 3.3px 3px #c78352,
+	3.3px 3.3px 3px #c78352,
+	3.4px 3.4px 3px #c78352,
+	3.5px 3.5px 3px #c78352,
+	3.5px 3.5px 3px #c78352,
+	3.6px 3.6px 3px #c78352,
+	3.7px 3.7px 3px #c78352,
+	3.9px 3.9px 3px #c78352,
+	4px 4px 3px #c78352,
+	4.1px 4.1px 3px #c78352,
+	4.2px 4.2px 3px #c78352,
+	4.3px 4.3px 3px #c78352,
+	4.4px 4.4px 3px #c78352,
+	4.5px 4.5px 3px #c78352,
+	4.6px 4.6px 3px #c78352,
+	4.7px 4.7px 3px #c78352,
+	4.8px 4.8px 3px #c78352,
+	4.9px 4.9px 3px #c78352,
+	5px 5px 3px #fff
+}
+	transition: 0.5s all ease-out;
+	&:hover {
+		background-color: chartreuse;
+		opacity:.5;
+		color: white;
+	};
+	
+	border-radius: 20px;
+    box-shadow: inset -13px -11px 17px 6px black;
+	padding: 1rem;
+    min-width: 250px;
+    width: 100%;
+    max-width: 750px;
+    margin: 2rem;
+
+}
 `;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
+import Luke from "../static/images/Luke_skywalker (1).jpg";
+
 export function Character(props) {
+	const [name, setName] = useState({firstName: "", LastName: ""});
+
 	return (
 		<CharacterCard>
-			<Header>Name:{props.character.name}</Header>
+			<Header>{props.character.name}</Header>
 			<CharacterInfo>
+				<CharacterImage src={Luke} alt="luke" />
+
 				<P>
 					<strong>Height:</strong>
 					{props.character.height}
@@ -40,63 +46,119 @@ export function Character(props) {
 				</P>
 				<P>
 					<strong>Rides:</strong>
-					{!props.character.vehicles[-1]?"🛴":`vehicles${props.character.vehicles[-1]}`}
-					{!props.character.starships[0]?"🛸":`starships:${props.character.starships[0]}`}
-					
+					{!props.character.vehicles[-1]
+						? "🛴"
+						: `vehicles${props.character.vehicles[-1]}`}
+					{!props.character.starships[0]
+						? "🛸"
+						: `starships:${props.character.starships[0]}`}
 				</P>
 				<P>
-					<strong>img:</strong>{props.character.url} 
-					<img src={props.character.url} alt=""></img>
+					<strong>img:</strong>
+
+					{props.character.url}
 				</P>
 			</CharacterInfo>
 		</CharacterCard>
 	);
 }
-const P=styled.p`
-display:flex;
-`
-const CharacterInfo=styled.div`
-padding-left: 24px;
-`
-const CharacterCard=styled.div`
-background: #fff;
-border-radius: 2px;
-display: flex;
-flex-direction: column;
-justify-content: space-evenly;
-height: auto;
-margin: 1rem auto;
-box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-width: auto;
-max-width: 500px;
-opacity: 0.85;
-text-shadow: 3px 2px 7px #702f1394;
-`
+const P = styled.p`
+	display: flex;
+`;
+const CharacterInfo = styled.div`
+	padding-left: 24px;
+	padding-bottom:24px;
+`;
+const CharacterImage = styled.img`
+	position: relative;
+	float: right;
+	border: tan 5px solid;
+	max-width: 350px;
+	width: 42%;
+	height: auto;
+	box-shadow: 1px 3px 10px 1px #7f7f6b;
+	right: 33px;
+	border-radius: 50px;
+`;
+const CharacterCard = styled.div`
+	background: #fff;
+	border-radius: 20px;
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	justify-content: space-between;
+	height: auto;
+	margin: 1rem;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+	width: auto;
+	max-width: 500px;
+	opacity: 0.85;
+	text-shadow: 3px 2px 7px #702f1394;
+	box-shadow: inset -11px -11px 20px 3px black;
+`;
 
 const Header = styled.header`
 	cursor: pointer;
-	font-size: 3rem;
+	font-size: 2rem;
 	border-radius: 10px;
 	color: #443e3e;
 	text-shadow: 1px 1px 5px #fff;
 	max-width: 100%;
 	z-index: 3;
-	text-shadow: 5px 5px 3px #fff, 4.9px 4.9px 3px #c78352,
-		4.8px 4.8px 3px #c78352, 4.7px 4.7px 3px #c78352,
-		4.6px 4.6px 3px #c78352, 4.5px 4.5px 3px #c78352,
-		4.4px 4.4px 3px #c78352, 4.3px 4.3px 3px #c78352,
-		4.2px 4.2px 3px #c78352, 4.1px 4.1px 3px #c78352, 4px 4px 3px #c78352,
-		3.9px 3.9px 3px #c78352, 3.5px 3.5px 3px #c78352,
-		3.3px 3.3px 3px #c78352, 3.2px 3.2px 3px #c78352,
-		3.7px 3.7px 3px #c78352, 3.6px 3.6px 3px #c78352,
-		3.5px 3.5px 3px #c78352, 3.4px 3.4px 3px #c78352,
-		3.3px 3.3px 3px #c78352, 3.2px 3.2px 3px #c78352,
-		3.1px 3.1px 3px #c78352, 3px 3px 3px #c78352, 2.9px 2.9px 3px #c78352;
+	padding:1rem;
+	text-shadow:
+	0.1px 0.1px 3px  #AAE,
+	0.2px 0.2px 3px  #AAD,
+	0.3px 0.3px 3px  #AAC,
+	0.4px 0.4px 3px  #AAB,
+	0.5px 0.5px 3px  #AAA,
+	0.6px 0.6px 3px  #AA9,
+	0.7px 0.7px 3px  #AA8,
+	0.8px 0.8px 3px  #AA7,
+	0.9px 0.9px 3px  #AA6,
+	0.0px 0.0px 3px  #AA5,
+	1.1px 1.1px 3px  #AA4,
+	1.2px 1.2px 3px  #AA3,
+	1.3px 1.3px 3px  #AA2,
+	1.4px 1.4px 3px  #AA1,
+	1.5px 1.5px 3px  #AA0,
+	1.6px 1.6px 3px  #ABF,
+	1.7px 1.7px 3px  #ABE,
+	1.8px 1.8px 3px  #ABD,
+	1.9px 1.9px 3px  #ABC,
+	2.5px 2.5px 3px  #ABB,
+	2.6px 2.6px 3px  #ABA,
+	2.7px 2.7px 3px  #AB9,
+	2.8px 2.8px 3px  #AB8,
+	2.9px 2.9px 3px  #AB6,
+	3.0px 3.0px 3px  #AB5,
+	3.1px 3.1px 3px  #AB4,
+	3.2px 3.2px 3px  #AB3,
+	3.3px 3.3px 3px  #AB2,
+	3.4px 3.4px 3px  #AB1,
+	3.5px 3.5px 3px  #AB0,
+	3.6px 3.6px 3px  #ACF,
+	3.7px 3.7px 3px  #ACE,
+	3.9px 3.9px 3px  #ACD,
+	4.0px 4.0px 3px  #ACC,
+	4.1px 4.1px 3px  #ACB,
+	4.2px 4.2px 3px  #ACA,
+	4.3px 4.3px 3px  #AC9,
+	4.4px 4.4px 3px  #AC8,
+	4.5px 4.5px 3px  #AC7,
+	4.6px 4.6px 3px  #AC6,
+	4.7px 4.7px 3px  #AC5,
+	4.8px 4.8px 3px  #AC4,
+	4.9px 4.9px 3px  #AC3,
+	8.0px 7.0px 3px black;
 	transition: 0.5s all ease-out;
 	&:hover {
-		background-color: palevioletred;
+		background-color: #8FF;
 		color: white;
 		opacity: 0.84;
-		margin-right: 15%;
-	}
+		margin-bottom: 15%;
+		border-radius: 20px;
+		box-shadow: inset -13px -11px 17px 6px black;
+	    padding: 1rem;
+	};
 `;
